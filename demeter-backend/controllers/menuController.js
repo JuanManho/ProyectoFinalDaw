5c492e04bdad4ea79d6e3b949677552a1bc2b2ab
JuanManho <150385006+JuanManho@users.noreply.github.com>
2024-12-07 20:20:46 +0100

    Incluimos demeter-delvery frontend

--- demeter-backend/controllers/menuController.js
+++ demeter-backend/controllers/menuController.js
@@ -12,41 +12,5 @@ const getMenuByRestaurantId = (req, res) => {
   });
 };
 
-// Crear un nuevo plato
-const createMenuItem = (req, res) => {
-  const newMenuItem = req.body;
-  Menu.create(newMenuItem, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: 'Error al crear el plato' });
-    } else {
-      res.status(201).json({ message: 'Plato creado', menuItemId: result.insertId });
-    }
-  });
-};
 
-// Actualizar un plato existente
-const updateMenuItem = (req, res) => {
-  const menuItemId = req.params.id;
-  const menuItemData = req.body;
-  Menu.update(menuItemId, menuItemData, (err) => {
-    if (err) {
-      res.status(500).json({ error: 'Error al actualizar el plato' });
-    } else {
-      res.json({ message: 'Plato actualizado' });
-    }
-  });
-};
-
-// Eliminar un plato
-const deleteMenuItem = (req, res) => {
-  const menuItemId = req.params.id;
-  Menu.delete(menuItemId, (err) => {
-    if (err) {
-      res.status(500).json({ error: 'Error al eliminar el plato' });
-    } else {
-      res.json({ message: 'Plato eliminado' });
-    }
-  });
-};
-
-module.exports = { getMenuByRestaurantId, createMenuItem, updateMenuItem, deleteMenuItem };
+module.exports = { getMenuByRestaurantId };
--- demeter-backend/routes/menuRoutes.js
+++ demeter-backend/routes/menuRoutes.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const {
-  getMenuByRestaurantId,
-  createMenuItem,
-  updateMenuItem,
-  deleteMenuItem,
-} = require('../controllers/menuController');
-
+const { getMenuByRestaurantId } = require('../controllers/menuController');
 const router = express.Router();
 
-router.get('/:id', getMenuByRestaurantId); // Obtener todos los platos de un restaurante
-router.post('/', createMenuItem); // Crear un plato
-router.put('/:id', updateMenuItem); // Actualizar un plato
-router.delete('/:id', deleteMenuItem); // Eliminar un plato
+router.get('/restaurant/:id', getMenuByRestaurantId); // Obtener menú de un restaurante específico
 
 module.exports = router;
